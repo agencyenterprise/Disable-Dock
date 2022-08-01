4a416249df3d33f191c4b40e48ac076ee775c727
Guilherme Welter <welter@ae.studio>
2022-08-01 14:54:52 -0300

    Initial Commit

---
+++ Diable Dock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Diable Dock
+//
+//  Created by Guilherme André Welter on 01/08/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
